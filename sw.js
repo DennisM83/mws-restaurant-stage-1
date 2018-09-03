f23757434b3d08d18d41dbeab8c94a9a188c131e
DennisM83 <dennisfinley@gmail.com>
2018-09-02 22:54:14 -0400

    added fetch to sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('install', function(event) {
     const urlToCache = [
+        '/',
         '/index.html',
         '/restaurant.html',
         '/css/styles.css',
@@ -25,11 +26,11 @@ self.addEventListener('install', function(event) {
     )
 });
 
-/*self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response){
             if(response) return response;
             return fetch(event.request);
         })
     )
-});*/
+});
